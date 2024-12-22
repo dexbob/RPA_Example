799f96ff85741d6b101bd8cec74b86952973c7cc
dexbob <mimacro@naver.com>
2024-12-22 23:47:54 +0900

    [오늘의_증권시황] 주요뉴스 파이썬 파일 수정 (URL변환 추가)

--- 21_오늘의_증권시황/Python/News.py
+++ 21_오늘의_증권시황/Python/News.py
@@ -1,12 +1,12 @@
+# conda install conda-forge::webdriver-manager
 
 import os
 import requests
 import pandas as pd
-from bs4 import BeautifulSoup as BSoup
 import win32com.client
+from bs4 import BeautifulSoup as BSoup
 
-
-def open_Excel(visible=False):
+def open_excel(visible=False):
     '''
     엑셀 열기
     - visible <bool> : 엑셀에 작업되는 상황 보이기 (기본값=False)
@@ -18,7 +18,7 @@ def open_Excel(visible=False):
     return excel
 
 
-def close_Excel(excel):
+def close_excel(excel):
     '''
     엑셀 닫기
     - excel <엑셀 객체> : 열었던 엑셀 객체
@@ -27,7 +27,26 @@ def close_Excel(excel):
     excel.Quit()
 
 
+
+def convert_url(url):
+    '''
+    URL에서 article_id와 office_id 추출하여 URL 설정
+    url <str> : 변환 대상 url 문자열
+    반환값 <str> : 변환된 url
+    '''
+    params = url.split('?')[-1]
+    dic = dict(param.split('=') for param in params.split('&'))
+    base_url = 'https://n.news.naver.com/mnews/article'
+    return f'{base_url}/{dic['office_id']}/{dic['article_id']}'
+
+
+
 def get_news_dataframe(day):
+    '''
+    대상 날짜의 네이버 증권 주요뉴스 추출
+    - day <str> : 대상 날짜 (yyyy-MM-dd)
+    - 반환값 <DataFrame> : 컬럼명 ('제목', 'URL', '내용', '언론사', '날짜')
+    '''
     url_base = 'https://finance.naver.com'
     url = url_base + f'/news/mainnews.naver?date={day}'
     
@@ -40,22 +59,33 @@ def get_news_dataframe(day):
         for news in soup.find_all('li', 'block1'):
             subject = news.find('dd', 'articleSubject')
             title = subject.text.strip()
-            link = url_base + subject.a['href']
+            org_url = url_base + subject.a['href']
+            url = convert_url(org_url)
             summary = news.find('dd', 'articleSummary')
             content = summary.contents[0].strip()
             press = summary.find('span', 'press').text
             date = summary.find('span', 'wdate').text
-            data.append([title, content, press, date, link])
-            # print(link, title, content, press, date, sep='\n')
+            data.append([title, url, content, press, date])
+            # print(title, org_url, url, content, press, date, sep='\n')
 
         page_num += 1
-        # 다음 페이지가 없으면 종료
+        # 다음 페이지가 없으면 반복 종료
         if soup.select_one('.pgRR') is None:
             break
-    return pd.DataFrame(data, columns=['제목', '내용', '언론사', '날짜', 'URL'])
+    return pd.DataFrame(data, columns=['제목', 'URL', '내용', '언론사', '날짜'])
+
 
 
 def insert_news_file(filePath, sheet, startRow, startCol, today):
+    '''
+    대상 날짜의 뉴스 파일 읽어서 엑셀 시트의 임의 위치에 쓰기 실행
+    - filePath <str> : 엑셀 파일 경로
+    - sheet <int | str> : 대상 파일 시트 (숫자는 시트인덱스로 1부터 시작, 문자는 시트명)
+    - startRow <int> : 셀의 시작행
+    - startCol <int> : 셀의 시작열
+    - today <str> : 오늘 날짜 (yyyy-MM-dd)
+    - 반환값 <str> : 처리 결과 문자열
+    '''
     try:
         df = get_news_dataframe(today)
         result = f'뉴스를 가져왔습니다. ({len(df)}행)'
@@ -66,7 +96,7 @@ def insert_news_file(filePath, sheet, startRow, startCol, today):
         print(result)
 
     try:
-        excel = open_Excel()
+        excel = open_excel()
         wb = excel.Workbooks.Open(filePath)
         ws = wb.Worksheets(sheet)
         # 제목 삽입
@@ -77,11 +107,11 @@ def insert_news_file(filePath, sheet, startRow, startCol, today):
             for c, value in enumerate(row, start=startCol):
                 ws.Cells(r, c).Value = value
         wb.Save()
-        result = f'{sheet}가 정상적으로 삽입되었습니다. ({today})'
+        result = f'{sheet}가 정상적으로 삽입되었습니다. ([{today}] {len(df)}행)'
     except Exception as e:
         result = f'에러 발생: {e}'
     finally:
-        close_Excel(excel)
+        close_excel(excel)
         print(result)
     return result
 
@@ -89,6 +119,10 @@ def insert_news_file(filePath, sheet, startRow, startCol, today):
 
 # 연습예제
 if __name__ == '__main__':
+    # day = '2024-12-22'
+    # news_df = get_news_dataframe(day)
+    # print(news_df)
+    
     fileFolder = "C:\\Users\\Dexter\\Source\\ALPACO8\\RPA_Example\\21_오늘의_증권시황\\Data\\Output"
-    filePath = os.path.join(fileFolder, "Today_Stock_Information_2024-12-20.xlsx")
-    insert_news_file(filePath, '주요뉴스', 3, 3, '2024-12-20')
+    filePath = os.path.join(fileFolder, "Today_Stock_Information_2024-12-22.xlsx")
+    insert_news_file(filePath, '주요뉴스', 1, 1, '2024-12-22')
